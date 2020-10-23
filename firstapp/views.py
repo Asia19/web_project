723015fa402849c577697469f48c7d9446b0cc46
asianasia19 <persiknast@yandex.ru>
2020-10-23 14:31:51 +0300

    blog home page creation has been started

---
+++ firstapp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import ListView
+from .models import Post
+
+# Create your views here.
+def index(request):
+    return HttpResponse("<h2>Главная</h2>")
+
+def about(request):
+    return HttpResponse("<h2>О сайте</h2>")
+
+
+class BlogListView(ListView):
+    model = Post
+    template_name = 'home.html'
