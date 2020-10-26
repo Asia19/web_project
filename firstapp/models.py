7b1ff6e39c00711726bb33020b5db8b997b63f01
asianasia19 <persiknast@yandex.ru>
2020-10-26 18:13:35 +0300

    tags under posts added

--- firstapp/models.py
+++ firstapp/models.py
@@ -1,23 +1,25 @@
 from django.db import models
+from django.utils import timezone
+from django.urls import reverse
 
 # Create your models here.
 
+class Tag(models.Model):
+    name = models.CharField(max_length=20, help_text="Enter a post tag")
+
+    def __str__(self):
+        return self.name
+
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
-    # image?
     author = models.ForeignKey(
         'auth.User',
         on_delete=models.CASCADE
     )
+    publication_date = models.DateTimeField(default=timezone.now, blank=True, null=True)
     body = models.TextField()
-    tag = models.ManyToManyField('Tag', help_text="Select a tag for this book")
+    tags = models.ManyToManyField('Tag', help_text="Select a tag for this book")
 
     def __str__(self):
         return self.title
-
-
-class Tag(models.Model):
-    name = models.CharField(max_length=20)
-
-    def __str__(self):
-        return self.name
--- firstapp/urls.py
+++ firstapp/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from firstapp import views
 from .views import BlogListView, BlogDetailView
 
 urlpatterns = [
