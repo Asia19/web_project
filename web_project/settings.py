f09e97f81f0b2cffc1c161d812ad4d9f21b17774
asianasia19 <persiknast@yandex.ru>
2020-11-05 22:55:05 +0300

    settings changed

--- web_project/settings.py
+++ web_project/settings.py
@@ -1,5 +1,6 @@
 import os
 import environ
+import dj_database_url
 from pathlib import Path
 from django.core.exceptions import ImproperlyConfigured
 
@@ -11,10 +12,10 @@ env = environ.Env(
 environ.Env.read_env()
 
 # False if not in os.environ
-DEBUG = env('DEBUG')
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 # Raises django's ImproperlyConfigured exception if SECRET_KEY not in os.environ
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # Parse database connection url strings like psql://user:pass@127.0.0.1:8458/db
 db_from_env = dj_database_url.config(conn_max_age=500)
