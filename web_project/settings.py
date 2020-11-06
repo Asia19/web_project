9582b53a73d97ce23a5da4040a3db6e97a947e06
asianasia19 <persiknast@yandex.ru>
2020-11-06 13:14:50 +0300

    settings changes

--- web_project/settings.py
+++ web_project/settings.py
@@ -126,6 +126,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
